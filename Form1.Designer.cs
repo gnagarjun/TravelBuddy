e39b4a094262cc4f56af4c83656b7a5af1fc5fee
Nagarjun Gopal <nagarjunmails@gmail.com>
2016-11-18 23:06:42 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace calHacks2016Traveler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.sourceTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.destTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateBox = new System.Windows.Forms.TextBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source";
+            // 
+            // sourceTextBox
+            // 
+            this.sourceTextBox.Location = new System.Drawing.Point(185, 135);
+            this.sourceTextBox.Name = "sourceTextBox";
+            this.sourceTextBox.Size = new System.Drawing.Size(127, 20);
+            this.sourceTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Destination";
+            this.label2.Visible = false;
+            // 
+            // destTextBox
+            // 
+            this.destTextBox.Location = new System.Drawing.Point(185, 171);
+            this.destTextBox.Name = "destTextBox";
+            this.destTextBox.Size = new System.Drawing.Size(127, 20);
+            this.destTextBox.TabIndex = 3;
+            this.destTextBox.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Give me a plan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(413, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(232, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Here you go with the options";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(669, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "SMART TRAVEL";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Save my time",
+            "Save my money",
+            "Help me explore places"});
+            this.comboBox1.Location = new System.Drawing.Point(185, 259);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(127, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(103, 259);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "priority";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(376, 115);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(319, 278);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(687, 446);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Let\'s go!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(94, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Date of travel";
+            // 
+            // dateBox
+            // 
+            this.dateBox.Location = new System.Drawing.Point(185, 205);
+            this.dateBox.Name = "dateBox";
+            this.dateBox.Size = new System.Drawing.Size(127, 20);
+            this.dateBox.TabIndex = 13;
+            this.dateBox.Text = "yyyy-mm-dd";
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(774, 115);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(344, 278);
+            this.listView2.TabIndex = 14;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Maiandra GD", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(858, 93);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(167, 19);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Trendy Destinations";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1247, 630);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.dateBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.destTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sourceTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sourceTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox destTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox dateBox;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.IO;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+using System.Web.Script.Serialization;
+using System.Xml;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Dynamic;
+
+namespace calHacks2016Traveler
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float distance = 0;
+            //string from = origin.Text;
+
+            string origin = sourceTextBox.Text;
+            string destination = destTextBox.Text;
+
+            string priority = comboBox1.SelectedItem.ToString();
+            string url=string.Empty;
+            if (string.Equals("Save my time", priority))
+            {
+                //Save my time
+                
+            }
+            if (string.Equals("Save my money", priority.ToString()))
+            {
+                //Save my money
+                url = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?origin=IST&destination=BOS&departure_date=2016-12-15&number_of_results=3&apikey=7qLFu6R2w4mhfPJvHk1SSdw9uiodewPJ";
+                string[] date = dateBox.Text.ToString().Split('-');
+                System.Net.WebClient wc = new WebClient();
+                byte[] response = wc.DownloadData(url);
+                string content = System.Text.Encoding.ASCII.GetString(response);
+
+                WebClient client = new WebClient();
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                dynamic item = serializer.Deserialize<object>(content);
+
+                List<string> listInfo = new List<string>();
+                int count = 0;
+                listView1.Items.Clear();
+                listView1.FullRowSelect = true;
+                listView1.View = View.Details;
+                listView1.Columns.Add("Option");
+                listView1.Columns.Add("Destination Airport");
+                listView1.Columns.Add("Ticket Price");
+                listView1.Columns.Add("Airlines");
+
+                foreach (Dictionary<string, object> d in item["results"])
+                {
+                    if (count == 10) { break; }
+                    ListViewItem lvi = new ListViewItem();
+                    foreach (var par in d)
+                    {
+                       // MessageBox.Show(par["itenaries"].ToString());
+                    }
+                    
+                   
+                }
+
+
+            }
+            if (string.Equals("Help me explore places", priority.ToString()))
+            {
+                //Help me explore places
+                string[] date = dateBox.Text.ToString().Split('-');
+                //sample working url  url = @"https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?apikey=7jKl9U9xtstyE6ftcAsGZw9DnqbeIjHs&origin=NYC&departure_date=2016-11-16";
+                url = @"https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?apikey=7jKl9U9xtstyE6ftcAsGZw9DnqbeIjHs&origin=" + origin + "&departure_date=" + date[0] + "-" + date[1] + "-" + date[2];
+                System.Net.WebClient wc = new WebClient();
+                byte[] response = wc.DownloadData(url);
+                string content = System.Text.Encoding.ASCII.GetString(response);
+
+                WebClient client = new WebClient();
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                dynamic item = serializer.Deserialize<object>(content);
+                // string name = item["name"];
+                List<string> listInfo = new List<string>();
+                int count = 0;
+                listView1.Items.Clear();
+                listView1.FullRowSelect = true;
+                listView1.View = View.Details;
+                listView1.Columns.Add("Option");
+                listView1.Columns.Add("Destination Airport");
+                listView1.Columns.Add("Ticket Price");
+                listView1.Columns.Add("Airlines");
+
+                foreach (Dictionary<string, object> d in item["results"])
+                {
+                    if (count == 10) { break; }
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = (count + 1).ToString();
+                    lvi.SubItems.Add(d["destination"].ToString());
+                    lvi.SubItems.Add(d["price"].ToString());
+                    lvi.SubItems.Add(d["airline"].ToString());
+                    listView1.Items.Add(lvi);
+                    count++;
+                }
+
+                //travel intelligence
+                string dateTime = DateTime.Today.ToString();
+                string[] dateNow = dateTime.Split(' ')[0].ToString().Split('/');
+
+
+                dateNow[2] = (Int32.Parse(dateNow[2]) - 1).ToString();        
+                url = @"https://api.sandbox.amadeus.com/v1.2/travel-intelligence/top-destinations?apikey=7jKl9U9xtstyE6ftcAsGZw9DnqbeIjHs&period="+dateNow[2]+"-"+dateNow[0]+"&origin="+origin;
+                response = wc.DownloadData(url);
+                content = System.Text.Encoding.ASCII.GetString(response);
+                serializer = new JavaScriptSerializer();
+                item = serializer.Deserialize<object>(content);
+                // string name = item["name"];
+                listInfo = new List<string>();
+                int trendyCount = 0;
+
+                listView2.Items.Clear();
+                listView2.FullRowSelect = true;
+                listView2.View = View.Details;
+                listView2.Columns.Add("Option");
+                listView2.Columns.Add("Trendy Destinations");
+                listView2.Columns.Add("Travelers last year");
+                foreach (Dictionary<string, object> d in item["results"])
+                {
+                    if (trendyCount == 10) { break; }
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = (trendyCount+1).ToString();
+                    lvi.SubItems.Add(d["destination"].ToString());
+                    lvi.SubItems.Add(d["travelers"].ToString());
+                    listView2.Items.Add(lvi);
+                    trendyCount++;
+                }
+                //travel intelligence
+
+                //worked before -> string url = "http://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&sensor=false";
+                //ser - worked  string url = @"https://maps.googleapis.com/maps/api/distancematrix/json?origins="+origin+"&destinations="+destination+"&key=AIzaSyAIw81_BYtUGphT4l_z8DuuPZ1xDK7n-BI";
+
+            }
+
+            //}
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 2)
+            {
+                destTextBox.ReadOnly = true;
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ticket booked with the selected option!");
+        }
+    }
+}
